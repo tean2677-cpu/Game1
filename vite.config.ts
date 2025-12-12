e3b528002a67aeecd9df4a1ec4d5f8bce6fdc72f
tean2677-cpu <tean2677@gmail.com>
2025-12-12 18:13:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/Game1/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
